516e14279ed401e5b2a585729c040149adb1ff9b
skaroweb <skaroweb@gmail.com>
2025-10-30 18:36:48 +0530

    fix: correct port and host for Render

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,8 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 10000),
+  url: env("RENDER_EXTERNAL_URL", "https://poppyshotel-backend.onrender.com/"),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
