87e1d46dc0558980f9a24f055ea1f4e650133c06
skaroweb <skaroweb@gmail.com>
2025-10-31 13:59:25 +0530

    Add create-admin script

--- scripts/create-admin.js
+++ scripts/create-admin.js
@@ -1,19 +1,28 @@
-// scripts/create-admin.js
-const { createAdminUser } = require("@strapi/strapi/lib/commands/admin");
+const { createStrapi } = require("@strapi/strapi");
+const strapi = createStrapi();
 
-(async () => {
-  try {
-    await createAdminUser({
-      email: process.env.STRAPI_ADMIN_CREATE_USER_EMAIL,
-      firstname: process.env.STRAPI_ADMIN_CREATE_USER_FIRSTNAME,
-      lastname: process.env.STRAPI_ADMIN_CREATE_USER_LASTNAME,
-      password: process.env.STRAPI_ADMIN_CREATE_USER_PASSWORD,
-      blocked: process.env.STRAPI_ADMIN_CREATE_USER_BLOCKED === "true",
+strapi.start().then(async () => {
+  const admin = {
+    firstname: "Admin",
+    lastname: "Poppy",
+    email: "admin@poppyshotels.com",
+    password: "Poppyshotel@123",
+  };
+
+  const superAdminExists = await strapi.db
+    .query("admin::user")
+    .findOne({ where: { email: admin.email } });
+
+  if (!superAdminExists) {
+    await strapi.admin.services.user.create({
+      ...admin,
+      roles: ["strapi-super-admin"],
       isActive: true,
-      roles: ["Super Admin"],
     });
-    console.log("✅ Admin user created successfully");
-  } catch (error) {
-    console.error("❌ Error creating admin user:", error);
+    console.log("✅ Super admin created successfully");
+  } else {
+    console.log("ℹ️ Super admin already exists");
   }
-})();
+
+  process.exit(0);
+});
