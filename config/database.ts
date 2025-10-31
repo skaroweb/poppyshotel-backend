c0e46e44c8c8f165f53b5fc1bfc6a7e21c2b60c4
skaroweb <skaroweb@gmail.com>
2025-10-31 15:45:56 +0530

    Fix database config for Render Postgres

--- config/database.ts
+++ config/database.ts
@@ -1,14 +1,27 @@
-module.exports = ({ env }) => ({
+import path from "path";
+
+export default ({ env }) => ({
   connection: {
-    client: env("DATABASE_CLIENT", "postgres"),
-    connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "poppys_strapi_db"),
-      user: env("DATABASE_USERNAME", "poppys_strapi_db_user"),
-      password: env("DATABASE_PASSWORD", "Poppyshotel@123"),
-      ssl: env.bool("DATABASE_SSL", true),
-    },
-    debug: false,
+    client: env("DATABASE_CLIENT", "sqlite"),
+    connection:
+      env("DATABASE_CLIENT") === "postgres"
+        ? {
+            host: env("DATABASE_HOST"),
+            port: env.int("DATABASE_PORT", 5432),
+            database: env("DATABASE_NAME"),
+            user: env("DATABASE_USERNAME"),
+            password: env("DATABASE_PASSWORD"),
+            ssl: env.bool("DATABASE_SSL", false)
+              ? { rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false) }
+              : false,
+          }
+        : {
+            filename: path.join(
+              __dirname,
+              "..",
+              env("DATABASE_FILENAME", ".tmp/data.db")
+            ),
+          },
+    useNullAsDefault: env("DATABASE_CLIENT") === "sqlite",
   },
 });
