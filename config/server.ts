aa6f828d963e05cb3c517947ec0f8f2104eff577
skaroweb <skaroweb@gmail.com>
2025-10-31 12:31:48 +0530

    Fix Render port and database config

--- config/server.ts
+++ config/server.ts
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 10000),
+  port: env.int("PORT", 10000), // Render requires this
   url: env("RENDER_EXTERNAL_URL", "https://poppyshotel-backend.onrender.com"),
   app: {
-    keys: env.array("APP_KEYS"),
+    keys: env.array("APP_KEYS", ["default-key-1", "default-key-2"]),
   },
 });
