bc398c56740eff8ece4f709818d7e07e6b504b98
skaroweb <skaroweb@gmail.com>
2025-10-31 23:07:02 +0530

    Config: SQLite local & Postgres internal for Render

--- config/database.ts
+++ config/database.ts
@@ -1,46 +1,16 @@
-// path: ./config/database.js
-
-const path = require("path");
-const parse = require("pg-connection-string").parse;
-
-module.exports = ({ env }) => {
-  const isProduction = env("NODE_ENV") === "production";
-
-  if (isProduction) {
-    const config = parse(env("DATABASE_URL"));
-
-    if (!config.host) {
-      throw new Error("DATABASE_URL is missing or invalid");
-    }
-
-    return {
-      connection: {
-        client: "postgres",
-        connection: {
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          user: config.user,
-          password: config.password,
-          ssl: { rejectUnauthorized: false },
+module.exports = ({ env }) => ({
+  connection:
+    process.env.NODE_ENV === "production"
+      ? {
+          client: "postgres",
+          connection: env("DATABASE_URL", ""),
+          useNullAsDefault: true,
+        }
+      : {
+          client: "sqlite",
+          connection: {
+            filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+          },
+          useNullAsDefault: true,
         },
-        debug: false,
-      },
-    };
-  }
-
-  // Local (SQLite)
-  return {
-    connection: {
-      client: "sqlite",
-      connection: {
-        filename: path.join(
-          __dirname,
-          "..",
-          env("DATABASE_FILENAME", ".tmp/data.db")
-        ),
-      },
-      useNullAsDefault: true,
-    },
-  };
-};
+});
